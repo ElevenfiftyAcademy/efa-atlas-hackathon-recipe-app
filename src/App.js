db157f47ed8e94dddd16de628f8fb98076cd43bf
Laurice Howard <lowkeylaurice@gmail.com>
2022-06-26 16:39:53 -0400

    recipe list started

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
         <div style={{display:"flex",justifyContent:"center",alignItems:"center"}} >
           <img src={logo} className="App-logo" alt="logo" width="100rem" height="100rem"/>
           <h1>Search Results</h1>
+          <ul data-testid="recipe-list">
+            <li data-testid="recipe-list-item">Orange Chicken</li>
+          </ul>
         </div>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,15 @@ test('renders search results header', () => {
   const linkElement = screen.getByText(/search results/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders recipe list', () => {
+  render(<App />);
+  const listElement = screen.getByTestId("recipe-list");
+  expect(listElement).toBeInTheDocument();
+})
+
+test('renders first recipe', () => {
+  render(<App />);
+  const listItemElement = screen.getByTestId("recipe-list-item");
+  expect(listItemElement).toBeInTheDocument();
+})
