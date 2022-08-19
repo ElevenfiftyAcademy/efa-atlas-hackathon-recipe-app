abd83ca2f54bb13dc3af62045e039950714e78c1
Martin Flaherty <flaherty.martin@gmail.com>
2022-08-19 13:02:45 -0400

    Adjusted logo positioning

--- src/App.js
+++ src/App.js
@@ -9,17 +9,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-        <div style={{display:"flex", flexDirection: "column"}} >
-         <div className="logo-images">
-            <img src={Watermelon} alt="watermelon" width="100rem" height="100rem" />
-            <img src={Flavorite} className="App-logo" alt="logo" width="100rem" height="100rem"/>
-          </div> 
-          <h1>Recipes</h1>
-          <ul data-testid="recipe-list" className="list-group">
-            { data.map(recipe => 
-              <li key={recipe.id} className="list-group-item">{recipe.title}</li>
-            )}
-          </ul>
+        <div className="container" >
+          <div className="row">
+            <div className="col">
+              <div className="logo-images">
+                <img src={Watermelon} alt="watermelon" style={{width:"8rem"}}  />
+                <img src={Flavorite} alt="logo" style={{width:"12.875rem"}}/>
+              </div> 
+              <h2>Recipes</h2>
+              <ul data-testid="recipe-list" className="list-group">
+                { data.map(recipe => 
+                  <li key={recipe.id} className="list-group-item">{recipe.title}</li>
+                )}
+              </ul>
+            </div>
+          </div>
         </div>
       </header>
     </div>
