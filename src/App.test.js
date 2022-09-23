77feda1002a34a4f1ceba071b54e59723e34e459
Lenny3000 <91618917+Lenny3000@users.noreply.github.com>
2022-09-23 12:56:19 -0400

    added a real call

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,35 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import data from './result.json';
 
-test('renders search results header', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Recipes/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders recipe list', () => {
-  render(<App />);
-  const listElement = screen.getByTestId("recipe-list");
-  expect(listElement).toBeInTheDocument();
-})
-
-test('renders first recipe', async () => {
-  render(<App />);
-  await waitFor(() => {
-    const listItemElement = screen.getByText(/Apple Or Peach Strudel/i);
-    expect(listItemElement).toBeInTheDocument();
+describe("App", () => {
+  beforeEach(() => {
+    jest.spyOn(global, "fetch").mockImplementation(() => {
+      return Promise.resolve({
+        json: () => Promise.resolve(data)
+      })
+    })
   })
+  test('renders search results header', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Recipes/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  
+  test('renders recipe list', () => {
+    render(<App />);
+    const listElement = screen.getByTestId("recipe-list");
+    expect(listElement).toBeInTheDocument();
+  })
+  
+  test('renders first recipe', async () => {
+    render(<App />);
+    await waitFor(() => {
+      const listItemElement = screen.getByText(/Apple Or Peach Strudel/i);
+      expect(listItemElement).toBeInTheDocument();
+    })
+  })
+  
 })
+
 
--- src/recipeService.js
+++ src/recipeService.js
@@ -1,11 +1,6 @@
-import data from './result.json';
-
-function findByIngredients() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(data);
-        }, 100);
-    })
+async function findByIngredients() {
+    let response = await fetch("https://api.spoonacular.com/recipes/findByIngredients?apiKey=26073aef38ae4bb9a8e0a5f932d4c334&ingredients=garlic");
+    return await response.json();
 }
 
 const recipeService = {
